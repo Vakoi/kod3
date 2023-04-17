fe194c73bce649864ef493bfd0228f7e50412b1f
Vakoi <mak-uk@mail.ru>
2023-04-17 16:05:12 +0300

    Добавьте файлы проекта.

---
+++ Практика день 1 (3)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 96; // замените на нужное значение
+
+        int firstDigit = number / 10;
+        int secondDigit = number % 10;
+
+        int sum = firstDigit + secondDigit;
+        int product = firstDigit * secondDigit;
+
+        Console.WriteLine("Сумма цифр: " + sum);
+        Console.WriteLine("Произведение цифр: " + product);
+    }
+}
